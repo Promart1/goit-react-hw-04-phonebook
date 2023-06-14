83076343303fb90aaef7ae7edb01368a3122dc74
Promart1 <marta.batih@gmail.com>
2023-06-14 23:56:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,21 @@ import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
 import { Filter } from './Filter/Filter';
 
-export const App = () => {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
+const contactsMenu = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? contactsMenu;
+  });
+
   const [filter, setFilter] = useState('');
 
   const addContact = (name, number) => {
-    // const { contacts } = this.state;
     const newContact = {
       id: nanoid(),
       name,
@@ -48,7 +42,6 @@ export const App = () => {
   };
 
   const getFilteredContacts = () => {
-    // const { contacts, filter } = this.state;
     const normalizedFilter = filter && filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
@@ -62,20 +55,9 @@ export const App = () => {
   };
 
   useEffect(() => {
-    const contactsLs = window.localStorage.getItem('contacts');
-    // const parsedContacts = JSON.parse(contactsLs);
-    if (contactsLs) {
-      setContacts(JSON.parse(contactsLs));
-    }
-  }, []);
-
-  useEffect(() => {
-    // if (this.state.contacts !== prevState.contacts) {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
-    // }
   }, [contacts]);
 
-  // const { filter } = this.state;
   const filteredContacts = filter ? getFilteredContacts() : contacts;
   return (
     <div className={css.container}>
